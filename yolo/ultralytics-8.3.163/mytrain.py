c6b96d445009dc72305c2c52ea736bca82a9191e
nooblong <18128966990@163.com>
2026-05-03 20:39:33 +0800

    11111111

--- yolo/ultralytics-8.3.163/mytrain.py
+++ yolo/ultralytics-8.3.163/mytrain.py
@@ -1,14 +1,16 @@
 from ultralytics import YOLO
 
 if __name__ == "__main__":
-    model = YOLO(r"yolo11n.pt")
+    model = YOLO(r"yolo11s.pt")
 
     model.train(
-        data=r"coco8.yaml",
-        epochs=10,
+        data=r"C:\Users\lyl\Documents\GitHub\misc\yolo\ultralytics-8.3.163\datasets\apex2\data.yaml",
+        epochs=100,
         batch=16,
-        imgsz=640,
-        workers=0,
-        cache=False,
-        amp=False
+        imgsz=416,
+        workers=8,
+        cache="disk",
+
+        degrees=0,
+        mixup=0.0,
     )
